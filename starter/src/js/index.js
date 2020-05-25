a974756925be0e626d733c2da8d27020e9d796dc
rallysport206 <phan.tony125@gmail.com>
2020-05-25 15:30:45 -0700

    adding some contollers

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,5 +1,35 @@
-// Global app controller
+import search from './models/Search';
+import * as searchView from './views/base';
+import { elements } from './views/base';
 
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module test.js! var x is ${x}`);
+/* Global state of the app
+- search object
+- current recipe object
+- shopping list object
+- liked recipes
+*/
+
+const state {};
+
+const controlSearch = async() => {
+    // 1 - get query from view
+    const query = searchView.getInput() //TODO
+
+    if (query) {
+        // 2 - New search object and add to state
+        state.search = new Search(query);
+
+        // 3 - prepare UI for results
+        searchView.clearInput();
+        searchView.clearResult();
+        // 4 - search for recipes
+        await state.search.getResults();
+        // 5 - Render results on UI
+        searchView.renderResult(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
